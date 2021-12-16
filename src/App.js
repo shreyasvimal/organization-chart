029442c01ed7879321125da58d8664aba47affd0
Vimal <vimal@helloinfinity.us>
2021-12-16 13:44:13 -0600

    Revert "Changes"
    This reverts commit 336a57bac1dfc6a9e999237d59645a05954dfe92.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useContext, useState, useCallback } from "react";
+import OrgContext from "./store/org-context";
+import OrganizationStructure from "./components/OrganizationStructure";
+import Popup from "./components/Popup";
 import MainHeader from "./components/MainHeader";
 
 import classes from "./App.module.css";
-import { Organization } from "./components/organization/Organization";
 
 const App = () => {
-  
+  //Global State and Actions
+  const { state, actions } = useContext(OrgContext);
+  // Setting a dummy state and forceUpdate to prevent rerender and a method to update on state change only.
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+  //Flag to open add popup
+  const [isOpen, setIsOpen] = useState(false);
+  //Parent Id to add reportee
+  const [parentId, setParentId] = useState({});
+  //Handle Popup Close
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const editPosition = (action, payload) => {
+    if (action === "addPosition") {
+      setParentId(payload);
+      setIsOpen(true);
+    } else {
+      actions({ type: action, payload: payload });
+      forceUpdate();
+    }
+  };
   return (
     <>
       <header>
         <MainHeader />
       </header>
       <div className={`${classes.organizationStructure}`}>
-        <Organization />
+        <OrganizationStructure data={state} editPosition={editPosition} />     
+        {isOpen && (
+          <Popup
+            handleClose={handleClose}
+            handleSave={editPosition}
+            parentId={parentId}
+          />
+        )}
+           
       </div>
     </>
   );
---
+++ src/components/OrganizationItem.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+
+import TextInput from "./Input";
+import ButtonComponent from "./Button";
+import classes from "./Organization-Item.module.css";
+
+const OrganizationItem = ({
+  item,
+  editPosition,
+  editFlag,
+  parentId,
+  handleClose,
+}) => {
+  const [showEdit, setShowEdit] = useState(editFlag);
+  const [name, setName] = useState(item.name);
+  const [title, setTitle] = useState(item.title);
+  const [startDate, setStartDate] = useState(item.startDate);
+  const [error, setError] = useState("");
+  
+  const reset = () => {
+    setName(item.name);
+    setTitle(item.title);
+    setStartDate(item.startDate);
+  };
+  const closeEdit = (type) => {
+    reset();
+    type === "cancel" ? setShowEdit(false) : handleClose();
+  };
+  const validate = () => {
+    let invalidArray = [];
+    if (name === "") invalidArray.push("Name");
+    if (title === "") invalidArray.push("Title");
+    invalidArray.length > 0
+      ? setError(invalidArray.join(",") + " is required")
+      : setError("");
+    return invalidArray.length === 0;
+  };
+  
+    const saveEdit = () => {
+    if (validate()) {
+      editPosition("edit", {
+        id: item.id,
+        name: name,
+        title: title,
+        startDate: startDate,
+      });
+      setShowEdit(false);
+    }
+  };
+  const addUser = () => {
+    if (validate()) {
+      editPosition("add", {
+        id: parentId,
+        name: name,
+        title: title,
+        startDate: startDate,
+      });
+      handleClose();
+    }
+  };
+  return (
+    <div className={`${classes.card}`}>
+      {showEdit ? (
+        <div className={`${classes.cardbody}`}>
+          <TextInput
+            id="name"
+            name="name"
+            label="Name"
+            defaultValue={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextInput
+            id="title"
+            name="title"
+            label="Designation"
+            defaultValue={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextInput
+            id="startDate"
+            name="startDate"
+            label="Start Date"
+            defaultValue={name}
+            onChange={(e) => setStartDate(e.target.value)}
+          />
+          <p className={`${classes.errorText}`}>{error}</p>     
+        </div>
+      ) : (
+        <div className={`${classes.cardbody}`}>
+          <h4>{name}</h4>
+          <p>
+            {title} {startDate ? ` - ${startDate}` : ""}
+          </p>
+        </div>
+      )}
+      <div className={`${classes.cardfooter}`}>
+        {showEdit ? (
+          <>
+            {editFlag ? (
+              <>
+                <ButtonComponent onClick={() => addUser()}>Add</ButtonComponent>
+                <ButtonComponent onClick={() => closeEdit("close")}>
+                  Cancel
+                </ButtonComponent>
+              </>
+            ) : (
+              <>
+                <ButtonComponent onClick={() => saveEdit()}>
+                  Add
+                </ButtonComponent>
+                <ButtonComponent onClick={() => closeEdit("cancel")}>
+                  Cancel
+                </ButtonComponent>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <ButtonComponent onClick={() => setShowEdit(true)}>
+              Edit
+            </ButtonComponent>
+
+            {item.id !== 0 && (
+              <>
+                <ButtonComponent
+                  onClick={() => editPosition("delete", { id: item.id })}
+                >
+                  Delete
+                </ButtonComponent>
+              </>
+            )}
+
+            <ButtonComponent
+              onClick={() => editPosition("addPosition", item.id)}
+            >
+              Add
+            </ButtonComponent>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+export default OrganizationItem;
